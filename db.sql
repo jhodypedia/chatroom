a0aadb1c8845d6622cb7ace7d11eb2083d330c17
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-07-07 19:14:14 +0700

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,11 +1,11 @@
 CREATE DATABASE IF NOT EXISTS nongchat;
-
 USE nongchat;
 
 CREATE TABLE messages (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(100),
+  sender VARCHAR(100),
+  receiver VARCHAR(100),
   content TEXT,
-  type ENUM('text', 'file') DEFAULT 'text',
+  type ENUM('text','file') DEFAULT 'text',
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
